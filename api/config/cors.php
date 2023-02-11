cc9b418390ed53722eb6f32b71f37daf4e854d23
gunsow <fukudatatsuya911@gmail.com>
2023-02-12 00:41:08 +0900

    ローカルで動作するDBの作成

--- api/config/cors.php
+++ api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', "http://localhost:3000")),
 
     'allowed_origins_patterns' => [],
 
