7dcc492ce37f24116a621d1f16b211e05840c7c1
greenthinktank <radionomoney@gmail.com>
2023-04-16 18:42:46 +0900

    sql文を発行する形にする。

--- api/app/UseCases/Matter/ListAction.php
+++ api/app/UseCases/Matter/ListAction.php
@@ -1,4 +1,7 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
 namespace App\UseCases\Matter;
 
 use App\Models\Matter;
@@ -12,12 +15,20 @@ class ListAction
      * 害獣情報一覧を取得する
      * @return \MatanYadaev\EloquentSpatial\SpatialBuilder 害獣情報一覧クエリ
      */
+    // public function __invoke()
+    // {
+    //     // とりあえずすべて取得
+    //     // TODO: 違和感のない程度で絞り込みなどをして、データ数が大きすぎないようにする必要がある
+    //     $list = Matter::orderBy('applied_at', 'asc');
+    //     return $list;
+    // }
+
+    // 全部のデータを取得すると多すぎて見にくいし、処理が重くなる原因になるので、取得するデータを制限して取得する（N＋１問題も避ける）
     public function __invoke()
     {
-        // とりあえずすべて取得
-        // TODO: 違和感のない程度で絞り込みなどをして、データ数が大きすぎないようにする必要がある
-        $list = Matter::orderBy('applied_at', 'asc');
-        return $list;
+        $query = Matter::select('matters.*')
+        ->orderBy('applied_at', 'asc');
+        return $query;
+
     }
 }
-
