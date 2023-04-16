cb03ad064d5b7d96d75c2097b1105d1f9d82a8f0
greenthinktank <radionomoney@gmail.com>
2023-04-16 17:04:33 +0900

    スペルミス修正。

--- api/app/Http/Controllers/MatterController.php
+++ api/app/Http/Controllers/MatterController.php
@@ -17,7 +17,7 @@ class MatterController extends Controller
     public function index(ListAction $action)
     {
         $list = $action();
-        $list->wirh([
+        $list->with([
             'userId',
             'location',
             'appliedAt',
