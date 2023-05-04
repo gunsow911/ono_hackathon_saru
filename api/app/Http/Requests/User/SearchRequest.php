228c3744f45c74f058c9d38a0b6485df38448882
greenthinktank <radionomoney@gmail.com>
2023-05-04 13:20:07 +0900

    user用のsearchrequestを作成しテストも作成し全passできた。

---
+++ api/app/Http/Requests/User/SearchRequest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Requests\User;
+
+use App\UseCases\User\ListEntity;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Carbon;
+
+class SearchRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'q' => 'string|max:255',
+            // 'from' => 'nullable|date_format:Y-m-d',
+            // 'to' => 'nullable|date_format:Y-m-d',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'q' => '検索文字',
+            // 'from' => '日付(自)',
+            // 'to' => '日付(至)',
+        ];
+    }
+
+    /**
+     * ユーザー情報検索データの生成
+     */
+    public function makeEntity(): ListEntity
+    {
+        return new ListEntity([
+            'query' =>  $this->input('q'),
+            // 'from' =>  $this->input('from') ? new Carbon($this->input('from')) : null,
+            // 'to' =>  $this->input('to') ? new Carbon($this->input('to')) : null,
+        ]);
+    }
+}
+
+
