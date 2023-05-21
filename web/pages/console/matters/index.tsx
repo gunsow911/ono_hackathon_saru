24dd5a35eef9a49d4bc63709b95e012fbadc65bd
greenthinktank <radionomoney@gmail.com>
2023-05-21 20:53:11 +0900

    選択削除ボタンを設置。

--- web/pages/console/matters/index.tsx
+++ web/pages/console/matters/index.tsx
@@ -6,6 +6,8 @@ import { Button, Card, Col, Row } from 'react-bootstrap'
 import MatterSearch from 'components/consoles/matters/MatterSearch'
 import { Condition } from 'hooks/console/matter/useGetMatterPage'
 import Link from 'next/link'
+import useRemoveMatter from 'hooks/console/matter/useRemoveMatter'
+import { toast } from 'react-toastify'
 
 const ConsoleMatterList: NextPageWithLayout = () => {
   const [condition, setCondition] = useState<Condition>({
@@ -15,6 +17,18 @@ const ConsoleMatterList: NextPageWithLayout = () => {
   })
   const onChange = (value: Condition) => {
     setCondition(value)
+  }
+
+  // const { data, isLoading, mutate } = useGetMatterPage(page, props.condition)
+  const { execute: executeRemove } = useRemoveMatter()
+
+  const onRemove = (matterId: string) => {
+    executeRemove(matterId).then((_) => {
+      // 削除後に獣害情報一覧を再取得する
+      // mutate()
+      toast.success('獣害情報を削除しました。')
+      props.onRemove && props.onRemove(matterId)
+    })
   }
 
   return (
@@ -36,6 +50,20 @@ const ConsoleMatterList: NextPageWithLayout = () => {
                   獣害情報新規作成
                 </Button>
               </Link>
+            </div>
+            <div className='mb-2'>
+            <Row>
+              <Col>
+                <Button
+                  size='sm'
+                  variant='danger'
+                  className='mx-1'
+                  onClick={() => onRemove(userId)}
+                >
+                  選択を削除
+                </Button>
+              </Col>
+            </Row>
             </div>
             <MatterTable condition={condition} />
           </Card>
