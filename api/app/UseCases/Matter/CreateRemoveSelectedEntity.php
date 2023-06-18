693785fc6c422ef037d5e0e8bbf2a5dbff13e959
greenthinktank <radionomoney@gmail.com>
2023-06-18 14:46:50 +0900

    参考も元に大幅に修正。

--- api/app/UseCases/Matter/CreateRemoveSelectedEntity.php
+++ api/app/UseCases/Matter/CreateRemoveSelectedEntity.php
@@ -1,66 +1,30 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\UseCases\Matter;
 
 use Illuminate\Support\Arr;
-use Illuminate\Support\Carbon;
 
 /**
- * 獣害情報(管理者)複数削除データ
+ * 獣害情報(管理者が実行)複数削除データ
  */
 class CreateRemoveSelectedEntity
 {
-    /** @var string ユーザid */
-    private $userId;
-    /** @var float 緯度 */
-    private $lat;
-    /** @var float 経度 */
-    private $lng;
-    /** @var Carbon 日付 */
-    private $appliedAt;
+    /** @var array<string> */
+    private $ids;
 
     public function __construct(array $data)
     {
-        $this->userId = Arr::get($data, 'user_id');
-        $this->lat = Arr::get($data, 'lat');
-        $this->lng = Arr::get($data, 'lng');
-        $this->appliedAt = new Carbon(Arr::get($data, 'applied_at'));
+        $this->ids = Arr::get($data, 'ids');
     }
 
     /**
-     * ユーザid
-     * @return string
+     * IDリスト
+     * @return array<string>
      */
-    public function getUserId()
+    public function getIds(): array
     {
-        return $this->userId;
-    }
-
-    /**
-     * 緯度
-     * @return float
-     */
-    public function getLat()
-    {
-        return $this->lat;
-    }
-
-    /**
-     * 経度
-     * @return float
-     */
-    public function getLng()
-    {
-        return $this->lng;
-    }
-
-    /**
-     * 日付
-     * @return Carbon
-     */
-    public function getAppliedAt()
-    {
-        return $this->appliedAt;
+        return $this->ids;
     }
 }
-
