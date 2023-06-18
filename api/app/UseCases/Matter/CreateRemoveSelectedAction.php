4609ae23d1fc191c12c7892ab3832424464a4e64
greenthinktank <radionomoney@gmail.com>
2023-06-18 14:53:50 +0900

    参考を元に大幅に変更。

---
+++ api/app/UseCases/Matter/CreateRemoveSelectedAction.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UseCases\Matter;
+
+use App\Models\Matter;
+use Illuminate\Support\Facades\DB;
+// use MatanYadaev\EloquentSpatial\Objects\Point;
+
+/**
+ * (管理者による)複数獣害情報一括削除アクション
+ */
+class CreateRemoveSelectedAction
+{
+    /** @var RemoveAction */
+    private $action;
+
+    public function __construct(RemoveAction $action)
+    {
+        $this->action = $action;
+    }
+
+    /**
+     * (管理者による)複数獣害情報一括削除をする
+     * @param CreateRemoveSelectedEntity $entity 複数害獣情報一斉削除データ
+     * @return Matter 獣害情報
+     */
+    public function __invoke(Matter $matter, CreateRemoveSelectedEntity $entity)
+    {
+        $matters = $matter->whereIn('id', $entity->getIds())->get();
+
+        return DB::transaction(function () use ($matters) {
+            /** @var Matter */
+            foreach ($matters as $matter) {
+                ($this->action)($matter);
+            }
+        });
+    }
+}
