250a102bbaa3a3332b3ca9c3dc570c6288dab960
greenthinktank <radionomoney@gmail.com>
2023-11-23 23:00:32 +0900

    管理者コンソールにも新設した。

--- web/components/consoles/matters/MatterForm.tsx
+++ web/components/consoles/matters/MatterForm.tsx
@@ -1,7 +1,19 @@
 import InputForm from 'components/atoms/InputForm'
 import LatLngMapForm from 'components/atoms/LatLngMapForm'
+import SelectForm from 'components/atoms/SelectForm'
 import React from 'react'
 import { Form } from 'react-bootstrap'
+
+  // 群れの規模選択肢
+  const single = { value: 'single', label: '単独' }
+  const group = { value: 'group', label: '群れ' }
+
+  // 出現時間帯選択肢
+  const earlymorning = { value: 'earlymorning', label: '早朝' }
+  const morning = { value: 'morning', label: '朝' }
+  const noon = { value: 'noon', label: '昼' }
+  const evening = { value: 'evening', label: '夕方' }
+  const night = { value: 'night', label: '夜' }
 
 const MatterForm = () => {
   return (
@@ -12,6 +24,20 @@ const MatterForm = () => {
       <Form.Group className='py-2'>
         <div style={{ height: '300px', position: 'relative' }}>
           <LatLngMapForm name='latLng' label='位置情報' />
+        </div>
+        <div className='pt-4'>
+          <SelectForm
+            name={'numberSelect'}
+            options={[single, group]}
+            label={'頭数'}
+          />
+        </div>
+        <div className='pt-4'>
+          <SelectForm
+            name={'timeSelect'}
+            options={[earlymorning, morning, noon, evening, night]}
+            label={'出現時間'}
+          />
         </div>
       </Form.Group>
     </>
